a8801a0dc489d188936c9de5cfc2dba236a2989a
her-cat <i@her-cat.com>
2020-12-05 17:07:04 +0800

    use 4 tab character

--- intset.c
+++ intset.c
@@ -33,11 +33,11 @@ static int le_intset;
 
 static uint8_t _intsetValueEncoding(int64_t v) {
 	if (v < INT32_MIN || v > INT32_MAX)
-		return INTSET_ENC_INT64;
+        return INTSET_ENC_INT64;
 	else if (v < INT16_MIN || v > INT16_MAX)
-		return INTSET_ENC_INT32;
+        return INTSET_ENC_INT32;
 	else
-		return INTSET_ENC_INT16;
+        return INTSET_ENC_INT16;
 }
 
 static int64_t _intsetGetEncoded(intset *is, int pos, uint8_t enc) {
@@ -46,14 +46,14 @@ static int64_t _intsetGetEncoded(intset *is, int pos, uint8_t enc) {
 	int16_t v16;
 
 	if (enc == INTSET_ENC_INT64) {
-		memcpy(&v64, ((int64_t *)is->contents)+pos, sizeof(v64));
-		return v64;
+        memcpy(&v64, ((int64_t *)is->contents)+pos, sizeof(v64));
+        return v64;
 	} else if (enc == INTSET_ENC_INT32) {
-		memcpy(&v32, ((int32_t *)is->contents)+pos, sizeof(v32));
-		return v32;
+        memcpy(&v32, ((int32_t *)is->contents)+pos, sizeof(v32));
+        return v32;
 	} else {
-		memcpy(&v16, ((int16_t *)is->contents)+pos, sizeof(v16));
-		return v16;
+        memcpy(&v16, ((int16_t *)is->contents)+pos, sizeof(v16));
+        return v16;
 	}
 }
 
@@ -63,11 +63,11 @@ static int64_t _intsetGet(intset *is, int pos) {
 
 static void _intsetSet(intset *is, int pos, int64_t value) {
 	if (is->encoding == INTSET_ENC_INT64) {
-		((int64_t *)is->contents)[pos] = value;
+        ((int64_t *)is->contents)[pos] = value;
 	} else if (is->encoding == INTSET_ENC_INT32) {
-		((int32_t *)is->contents)[pos] = value;
+        ((int32_t *)is->contents)[pos] = value;
 	} else {
-		((int16_t *)is->contents)[pos] = value;
+        ((int16_t *)is->contents)[pos] = value;
 	}
 }
 
@@ -82,45 +82,45 @@ static uint8_t intsetSearch(intset *is, uint64_t value, uint32_t *pos) {
 
 	/* intset 为空直接返回 0 */
 	if (is->length == 0) {
-		if (pos) *pos = 0;
-		return 0;
+        if (pos) *pos = 0;
+        return 0;
 	} else {
-		/* value 大于 intset 中最大的值时，pos 为 inset 的长度，
-		 * value 小于 intset 中最小的值时，post 为 0，
-		 * 不管是大于还是小于，都需要对 intset 进行扩容。 */
-		if (value > _intsetGet(is, max)) {
-			if (pos) *pos = is->length;
-			return 0;
-		} else if (value < _intsetGet(is, 0)) {
-			*pos = 0;
-			return 0;
-		}
+        /* value 大于 intset 中最大的值时，pos 为 inset 的长度，
+         * value 小于 intset 中最小的值时，post 为 0，
+         * 不管是大于还是小于，都需要对 intset 进行扩容。 */
+        if (value > _intsetGet(is, max)) {
+        	if (pos) *pos = is->length;
+        	return 0;
+        } else if (value < _intsetGet(is, 0)) {
+        	*pos = 0;
+        	return 0;
+        }
 	}
 
 	/* 二分查找法确定 value 是否在 intset 中。 */
 	while (max >= min) {
-		/* 右移一位表示除以二。 */
-		mid = ((unsigned int)min + (unsigned int)max) >> 1;
-		/* 获取处于 mid 的值。 */
-		cur = _intsetGet(is, mid);
-		/* value > cur 时，表示 value 可能在右边值更大的区域，
-		 * value < cur 时，表示 value 可能在左边值更小的区域，
-		 * 其他情况说明找到了 value 所在的位置。 */
-		if (value > cur) {
-			min = mid + 1;
-		} else if (value < cur) {
-			max = mid - 1;
-		} else {
-			break;
-		}
+        /* 右移一位表示除以二。 */
+        mid = ((unsigned int)min + (unsigned int)max) >> 1;
+        /* 获取处于 mid 的值。 */
+        cur = _intsetGet(is, mid);
+        /* value > cur 时，表示 value 可能在右边值更大的区域，
+         * value < cur 时，表示 value 可能在左边值更小的区域，
+         * 其他情况说明找到了 value 所在的位置。 */
+        if (value > cur) {
+        	min = mid + 1;
+        } else if (value < cur) {
+        	max = mid - 1;
+        } else {
+        	break;
+        }
 	}
 
 	if (value == cur) {
-		if (pos) *pos = mid;
-		return 1;
+        if (pos) *pos = mid;
+        return 1;
 	} else {
-		if (pos) *pos = min;
-		return 0;
+        if (pos) *pos = min;
+        return 0;
 	}
 }
 
@@ -137,13 +137,13 @@ static intset *intsetUpgradeAndAdd(intset *is, int64_t value) {
 	/* 从后向前升级，这样不会覆盖旧值。
 	 * 注意，prepend 变量用于确保 intset 开头或结尾有一个空位，用来存储新值 */
 	while (length--)
-		_intsetSet(is, length + prepend, _intsetGetEncoded(is, length, curenc));
+        _intsetSet(is, length + prepend, _intsetGetEncoded(is, length, curenc));
 
 	/* 在 intset 开头或者末尾处设置新值。 */
 	if (prepend)
-		_intsetSet(is, 0, value);
+        _intsetSet(is, 0, value);
 	else
-		_intsetSet(is, is->length, value);
+        _intsetSet(is, is->length, value);
 
 	is->length = is->length + 1;
 	return is;
@@ -155,17 +155,17 @@ static void intsetMoveTail(intset *is, uint32_t from, uint32_t to) {
 	uint32_t encoding = is->encoding;
 
 	if (encoding == INTSET_ENC_INT64) {
-		src = (int64_t *)is->contents + from;
-		dst = (int64_t *)is->contents + to;
-		bytes *= sizeof(int64_t);
+        src = (int64_t *)is->contents + from;
+        dst = (int64_t *)is->contents + to;
+        bytes *= sizeof(int64_t);
 	} else if (encoding == INTSET_ENC_INT32) {
-		src = (int32_t *)is->contents + from;
-		dst = (int32_t *)is->contents + to;
-		bytes *= sizeof(int32_t);
+        src = (int32_t *)is->contents + from;
+        dst = (int32_t *)is->contents + to;
+        bytes *= sizeof(int32_t);
 	} else {
-		src = (int16_t *)is->contents + from;
-		dst = (int16_t *)is->contents + to;
-		bytes *= sizeof(int16_t);
+        src = (int16_t *)is->contents + from;
+        dst = (int16_t *)is->contents + to;
+        bytes *= sizeof(int16_t);
 	}
 	memmove(dst, src, bytes);
 }
@@ -173,7 +173,7 @@ static void intsetMoveTail(intset *is, uint32_t from, uint32_t to) {
 static void intsetDestroy(zend_resource *rsrc TSRMLS_DC){
 	intset *is = (intset *) rsrc->ptr;
 	if (is->length > 0) {
-		efree(is->contents);
+        efree(is->contents);
 	}
 	efree(is);
 }
@@ -192,15 +192,15 @@ intset *intsetAdd(intset *is, int64_t value, uint8_t *success) {
 	if (success) *success = 1;
 
 	if (valuec > is->encoding) {
-		return intsetUpgradeAndAdd(is, value);
+        return intsetUpgradeAndAdd(is, value);
 	} else {
-		if (intsetSearch(is, value, &pos)) {
-			if (success) *success = 0;
-			return is;
-		}
+        if (intsetSearch(is, value, &pos)) {
+        	if (success) *success = 0;
+        	return is;
+        }
 
-		is = intsetResize(is, is->length + 1);
-		if (pos < is->length) intsetMoveTail(is, pos, pos + 1);
+        is = intsetResize(is, is->length + 1);
+        if (pos < is->length) intsetMoveTail(is, pos, pos + 1);
 	}
 
 	_intsetSet(is, pos, value);
@@ -213,13 +213,13 @@ intset *intsetRemove(intset *is, int64_t value, uint8_t *success) {
 	uint32_t pos;
 
 	if (valenc <= is->encoding && intsetSearch(is, value, &pos)) {
-		uint32_t len = is->length;
+        uint32_t len = is->length;
 
-		if (success) *success = 1;
+        if (success) *success = 1;
 
-		if (pos < (len - 1)) intsetMoveTail(is, pos + 1, pos);
-		is = intsetResize(is, len - 1);
-		is->length = len - 1;
+        if (pos < (len - 1)) intsetMoveTail(is, pos + 1, pos);
+        is = intsetResize(is, len - 1);
+        is->length = len - 1;
 	}
 	return is;
 }
@@ -235,8 +235,8 @@ int64_t intsetRandom(intset *is) {
 
 uint8_t intsetGet(intset *is, uint32_t pos, int64_t *value) {
 	if (pos < is->length) {
-		*value = _intsetGet(is, pos);
-		return 1;
+        *value = _intsetGet(is, pos);
+        return 1;
 	}
 	return 0;
 }
@@ -259,7 +259,7 @@ PHP_FUNCTION(intset_create)
 
 	is = intsetNew();
 	if (!is) {
-		RETURN_NULL()
+        RETURN_NULL()
 	}
 
 	RETURN_RES(zend_register_resource(is, le_intset))
@@ -276,16 +276,16 @@ PHP_FUNCTION(intset_add)
 	intset *is = NULL;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &isp, &value) == FAILURE)
-		return;
+        return;
 
 	if ((is = zend_fetch_resource(Z_RES_P(isp), INTSET_TYPE, le_intset)) == NULL) {
-		RETURN_FALSE
+        RETURN_FALSE
 	}
 
 	intsetAdd(is, value, &success);
 
 	if (success) {
-		RETURN_TRUE
+        RETURN_TRUE
 	}
 
 	RETURN_FALSE
@@ -302,16 +302,16 @@ PHP_FUNCTION(intset_remove)
 	intset *is = NULL;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &isp, &value) == FAILURE)
-		return;
+        return;
 
 	if ((is = zend_fetch_resource(Z_RES_P(isp), INTSET_TYPE, le_intset)) == NULL) {
-		RETURN_FALSE
+        RETURN_FALSE
 	}
 
 	intsetRemove(is, value, &success);
 
 	if (success) {
-		RETURN_TRUE
+        RETURN_TRUE
 	}
 
 	RETURN_FALSE
@@ -327,14 +327,14 @@ PHP_FUNCTION(intset_find)
 	intset *is = NULL;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &isp, &value) == FAILURE)
-		return;
+        return;
 
 	if ((is = zend_fetch_resource(Z_RES_P(isp), INTSET_TYPE, le_intset)) == NULL) {
-		RETURN_FALSE
+        RETURN_FALSE
 	}
 
 	if (intsetFind(is, value)) {
-		RETURN_TRUE
+        RETURN_TRUE
 	}
 
 	RETURN_FALSE
@@ -349,10 +349,10 @@ PHP_FUNCTION(intset_random)
 	intset *is = NULL;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &isp) == FAILURE)
-		return;
+        return;
 
 	if ((is = zend_fetch_resource(Z_RES_P(isp), INTSET_TYPE, le_intset)) == NULL) {
-		RETURN_FALSE
+        RETURN_FALSE
 	}
 
 	RETURN_LONG(intsetRandom(is))
@@ -368,14 +368,14 @@ PHP_FUNCTION(intset_get)
 	intset *is = NULL;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &isp, &pos) == FAILURE)
-		return;
+        return;
 
 	if ((is = zend_fetch_resource(Z_RES_P(isp), INTSET_TYPE, le_intset)) == NULL) {
-		RETURN_FALSE
+        RETURN_FALSE
 	}
 
 	if (intsetGet(is, pos, &value)) {
-		RETURN_LONG(value)
+        RETURN_LONG(value)
 	}
 
 	RETURN_FALSE
@@ -390,10 +390,10 @@ PHP_FUNCTION(intset_len)
 	intset *is = NULL;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &isp) == FAILURE)
-		return;
+        return;
 
 	if ((is = zend_fetch_resource(Z_RES_P(isp), INTSET_TYPE, le_intset)) == NULL) {
-		RETURN_FALSE
+        RETURN_FALSE
 	}
 
 	RETURN_LONG(intsetLen(is))
@@ -408,10 +408,10 @@ PHP_FUNCTION(intset_blob_len)
 	intset *is = NULL;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &isp) == FAILURE)
-		return;
+        return;
 
 	if ((is = zend_fetch_resource(Z_RES_P(isp), INTSET_TYPE, le_intset)) == NULL) {
-		RETURN_FALSE
+        RETURN_FALSE
 	}
 
 	RETURN_LONG(intsetBlobLen(is))
@@ -491,7 +491,7 @@ zend_module_entry intset_module_entry = {
 	intset_functions,
 	PHP_MINIT(intset),
 	PHP_MSHUTDOWN(intset),
-	PHP_RINIT(intset),		/* Replace with NULL if there's nothing to do at request start */
+	PHP_RINIT(intset),        /* Replace with NULL if there's nothing to do at request start */
 	PHP_RSHUTDOWN(intset),	/* Replace with NULL if there's nothing to do at request end */
 	PHP_MINFO(intset),
 	PHP_INTSET_VERSION,
